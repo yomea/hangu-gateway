aff27b20f0a0fe0059169d0e219e329a52c908d2
wuzhenhong <wuzhenhong@banksteel.com>
2024-03-22 14:58:19 +0800

    feat：接入hangu系列注册中心

--- src/main/java/org/hangu/gateway/Bootstrap.java
+++ src/main/java/org/hangu/gateway/Bootstrap.java
@@ -2,12 +2,18 @@ package org.hangu.gateway;
 
 import com.hangu.rpc.common.properties.HanguProperties;
 import com.hangu.rpc.common.properties.ZookeeperConfigProperties;
+import com.hangu.rpc.common.registry.HanguRegistryService;
 import com.hangu.rpc.common.registry.RegistryService;
 import com.hangu.rpc.common.registry.ZookeeperRegistryService;
+import java.util.Collections;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import org.hangu.center.discover.client.DiscoverClient;
+import org.hangu.center.discover.config.impl.ClientResponseHandlerConfigDefaultImpl;
+import org.hangu.center.discover.properties.ClientProperties;
+import org.hangu.center.discover.starter.CenterClientStarter;
 import org.hangu.gateway.server.NettyServer;
 
 /**
@@ -19,7 +25,7 @@ public class Bootstrap {
     public static void main(String[] args) {
         NettyServer.start(8099,
             Bootstrap.buildWorkExecutor(),
-            Bootstrap.buildZkRegistryService(),
+            Bootstrap.buildHanguRegistryService(),
             Bootstrap.buildHanguProperties());
     }
 
@@ -34,6 +40,13 @@ public class Bootstrap {
         ZookeeperConfigProperties properties = new ZookeeperConfigProperties();
         properties.setHosts("192.168.203.233:2181");
         return new ZookeeperRegistryService(properties);
+    }
+
+    private static RegistryService buildHanguRegistryService() {
+        ClientProperties clientProperties = new ClientProperties();
+        clientProperties.setPeerNodeHosts("localhost:9991,localhost:9992");
+        DiscoverClient discoverClient = CenterClientStarter.start(clientProperties, Collections.singletonList(new ClientResponseHandlerConfigDefaultImpl()));
+        return new HanguRegistryService(discoverClient);
     }
 
     private static HanguProperties buildHanguProperties() {
